99d31366bf2bb6230317461a9261d05505192285
luxiansheng <luxiansheng@zto.cn>
2019-06-14 20:26:45 +0800

    optimize lint and format scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  '*.(js|vue)': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.(css|less|sass|scss|vue|htm|html)': [
-    'stylelint --fix',
+  '*.(js|vue)': ['eslint --fix --cache', 'prettier --write', 'git add'],
+  '*.(vue|htm|html|css|less|sass|scss|styl|stylus|md|js)': [
+    'stylelint --fix --cache',
     'prettier --write ',
     'git add'
   ],
